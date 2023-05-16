bd1930f6f83b028429f245df483a8075b10395cf
Rohit Chaudhari <chaudharirohit667@gmail.com>
2023-05-16 13:30:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const App = () => {
+    let time = new Date().toLocaleTimeString();
+
+    const [ctime, setCtime] = useState(time);
+
+    const UpdateTime = () => {
+        time = new Date().toLocaleTimeString();
+        setCtime(time);
+    };
+
+    setInterval(UpdateTime, 1000);
+    return (
+        <>
+            <h1> {ctime} </h1>
+        </>
+    );
+};
+
+export default App;
